34ffc2a822fc7aa47b6482fd345a7c7616c56665
techoutcomes <127057485+techoutcomes@users.noreply.github.com>
2023-08-01 21:21:34 +0100

    Update main.tf

--- sessions/300723/simplevpc-ec2/web-tier/ec2/main.tf
+++ sessions/300723/simplevpc-ec2/web-tier/ec2/main.tf
@@ -101,15 +101,6 @@ resource "aws_security_group" "ec2_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    ##cidr_blocks = ["0.0.0.0/0"]
-    security_groups = [aws_security_group.lb_sg.id]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
